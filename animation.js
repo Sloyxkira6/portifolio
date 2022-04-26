8ab921a4955e79bdfc4ea38a30c608b1ef3a8a0e
gabriel silva <gabriel.christ.silva@gmail.com>
2022-04-26 18:22:21 -0300

    alteracao2

--- animation.js
+++ animation.js
@@ -1,18 +1,45 @@
-const imgs = document.getElementById("img")
-const img = document.querySelectorAll("img")
+let esquerdo = document.getElementById('botesrolagem1')
+let carrossel = document.getElementById('carrossel')
 
-let id= 0
 
-function carrossel () {
-    id++
-
-    if(id > img.length - 1){
-        id = 0
+let cont = 0
+let im1 = [
+    document.getElementById('itens1'),
+    document.getElementById('itens2'),
+    document.getElementById('itens3'),
+    document.getElementById('itens4'),
+    document.getElementById('itens5'),
+    document.getElementById('itens6')
+]
+esquerdo.addEventListener('click', (event)=> {
+    cont++
+    if(cont==im1.length) {
+        cont=0
     }
-    imgs.style.transform = `translateX(${-id * 500}px)`
-}
+    window.alert(`${cont}`)
+    carrossel.fillRect
+    carrossel.drawImage(im1[cont], 500, 250, 500, 250)
+    
 
-setInterval(carrossel, 1800)
+})
+
+
+
+// const imgs = document.getElementById("img")
+// const img = document.querySelectorAll("img")
+
+// let id= 0
+
+// function carrossel () {
+//     id++
+
+//     if(id > img.length - 1){
+//         id = 0
+//     }
+//     imgs.style.transform = `translateX(${-id * 500}px)`
+// }
+
+// setInterval(carrossel, 1800)
     
 
 
